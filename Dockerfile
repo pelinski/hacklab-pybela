2d4b665cb6bc102166efa8278dca0793bed2309f
Teresa Pelinski <teresapelinski@gmail.com>
2025-06-09 20:46:26 +0100

    update

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,21 @@
 FROM pelinski/xc-bela-container:v1.1.0
 
-RUN apt-get update && \
-      apt-get install -y python3 pip
-            
-COPY code/requirements.txt ./
-RUN pip install -r requirements.txt
-
 COPY docker-scripts/download_libtorch.sh ./
 RUN ./download_libtorch.sh  && rm download_libtorch.sh
 
 # rev C
 RUN git clone https://github.com/BelaPlatform/bb.org-overlays.git /sysroot/opt/bb.org-overlays
 
-RUN mkdir -p /root/code
+RUN apt-get update && \
+      apt-get install -y python3 pip
+
+RUN pip install pybela==2.0.3
+RUN pip install torch --index-url https://download.pytorch.org/whl/cu117 
+
+RUN mkdir -p /root/hacklab
 
 RUN git clone --recurse-submodules -j8  https://github.com/pelinski/hacklab-pybela /root/code/ 
 
-WORKDIR /root/code
+WORKDIR /root/hacklab
 
 CMD /bin/bash
--- code/bela-code/dataset-capture/render.cpp
+++ code/bela-code/dataset-capture/render.cpp
@@ -1,38 +1,86 @@
 #include <Bela.h>
 #include <Watcher.h>
+#include <cmath>
 #include <libraries/Scope/Scope.h>
+#include <libraries/Biquad/Biquad.h>
+Biquad hpFilter;	// Biquad high-pass frequency;
 
+#define NUM_SENSORS 7 // Number of sensors
 
-Watcher<float> wIn("wIn");
+unsigned int gAudioFramesPerAnalogFrame; // Number of audio frames per analog
+                                         // frame
+float gInvSampleRate;                    // 1/sample rate
+float gInvAudioFramesPerAnalogFrame;     // 1/audio frames per analog frame
+
+// Vector of Watcher pointers
+std::vector<Watcher<float>*> gPiezos;
+float gIn[NUM_SENSORS];
+float gGain;
+
 Scope scope;
 
 
-bool setup(BelaContext *context, void *userData) {
+bool setup(BelaContext* context, void* userData) {
+    Bela_getDefaultWatcherManager()->getGui().setup(context->projectName);
+    Bela_getDefaultWatcherManager()->setup(context->audioSampleRate); // set sample rate in watcher
+    gAudioFramesPerAnalogFrame = context->audioFrames / context->analogFrames;
+    gInvAudioFramesPerAnalogFrame = 1.0f / gAudioFramesPerAnalogFrame;
+    gInvSampleRate = 1.0f / context->audioSampleRate;
 
-  Bela_getDefaultWatcherManager()->getGui().setup(context->projectName);
-  Bela_getDefaultWatcherManager()->setup(
-      context->audioSampleRate); // set sample rate in watcher
+    // Initialize the Watcher pointers and add them to the vector
+    for (unsigned int i = 0; i < NUM_SENSORS; ++i) {
+        gPiezos.push_back(new Watcher<float>("piezo_" + std::to_string(i + 1)));
+    }
 
-  scope.setup(2, context->audioSampleRate);
+    // Initialize the scope
+    scope.setup(9, context->analogSampleRate);
 
-  return true;
+    Biquad::Settings settings{
+        .fs = context->audioSampleRate,
+        .type = Biquad::highpass,
+        .cutoff = 2.0,
+        .q = 0.707,
+        .peakGainDb = 0,
+        };
+    hpFilter.setup(settings);
+    return true;
 }
 
-void render(BelaContext *context, void *userData) {
+void render(BelaContext* context, void* userData) {
+    for (unsigned int n = 0; n < context->audioFrames; n++) {
+        uint64_t frames = context->audioFramesElapsed + n;
+        Bela_getDefaultWatcherManager()->tick(frames);
 
-  for (unsigned int n = 0; n < context->audioFrames; n++) {
-    uint64_t frames = context->audioFramesElapsed + n;
-    Bela_getDefaultWatcherManager()->tick(frames);
+        if (gAudioFramesPerAnalogFrame && !(n % gAudioFramesPerAnalogFrame)) {
+            // read analog inputs and update frequency and amplitude
+            // Depending on the sampling rate of the analog inputs, this will
+            // happen every audio frame (if it is 44100)
+            // or every two audio frames (if it is 22050)
+            for (unsigned int i = 0; i < NUM_SENSORS; i++) {
+                *gPiezos[i] =  analogRead(context, n / gAudioFramesPerAnalogFrame, i);
+                // gAmplitude[i] = gPiezos[i]->get();
+            }
+            gGain = analogRead(context, n / gAudioFramesPerAnalogFrame, NUM_SENSORS);
 
-    float _in = audioRead(context, n, 0);
-    wIn = _in; // update watcher with input value
-    float _out = _in;
+            float in = audioRead(context, n, 0); // Read audio input from channel 0
 
-    audioWrite(context, n, 0, _out);
-    audioWrite(context, n, 1, _out);
+            // audio output
+            float out = 0.0;
+            for (unsigned int i = 0; i < NUM_SENSORS; i++) {
+              out += gPiezos[i].get(); // Scale the input to a reasonable output level
+            }
+            // out *= 0.4; // Scale down to avoid clipping
+            out = hpFilter.process(out); // Apply high-pass filter
+            for (unsigned int channel = 0; channel < context->audioOutChannels; channel++) {
+              audioWrite(context, n, channel, out);
+            }
+            scope.log(gIn[0], gIn[1], gIn[2], gIn[3], gIn[4], gIn[5], gIn[6], gGain, out);
+          }
 
-    scope.log(_in,_out); // scope doesn't like watched variables
-  }
+
+
+    }
 }
 
-void cleanup(BelaContext *context, void *userData) {}
+void cleanup(BelaContext* context, void* userData) {
+}
